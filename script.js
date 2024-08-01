9cce9c6bc603e017a27b60b59074166d777ebd55
Rafael <rcarneiro782@gmail.com>
2024-08-01 16:00:01 -0300

    clima/segunda parte & final

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
-    event.preventDefault();
+document.querySelector(".busca").addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    let input = document.querySelector('#searchInput').value;
+  let input = document.querySelector("#searchInput").value;
 
-    if(input !== ''){
-     showWarning('Carregando....')
+  if (input !== "") {
+    clearInfo();
+    showWarning("Carregando....");
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+      input
+    )}&appid=2ee2f14b057d78ca47fe4ea94d243955&units=metric&lang=pt_br`;
+
+    let results = await fetch(url);
+    let json = await results.json();
+
+    if (json.cod === 200) {
+      showInfor({
+        name: json.name,
+        country: json.sys.country,
+        temp: json.main.temp,
+        tempIcon: json.weather[0].icon,
+        windSpeed: json.wind.speed,
+        windAngle: json.wind.deg,
+      });
+    } else {
+      clearInfo();
+      showWarning("Não econtramos esta localização");
     }
-
+  } else {
+    clearInfo();
+  }
 });
 
-function showWarning(msg){
-    document.querySelector('.aviso').innerHTML = msg;
+function showInfor(json) {
+  showWarning("");
+
+  document.querySelector(
+    ".titulo"
+  ).innerHTML = `${json.name}, ${json.country} `;
+  document.querySelector(".tempInfo").innerHTML = `${json.temp} <sup>ºC</sup>`;
+  document.querySelector(
+    ".ventoInfo"
+  ).innerHTML = `${json.windSpeed} <span>KM/h</span>`;
+
+  document
+    .querySelector(".temp img")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`
+    );
+
+  document.querySelector(".ventoPonto").style.transform = `rotate(${
+    json.windAngle - 90
+  }deg)`;
+  document.querySelector(".resultado").style.display = "block";
+}
+
+function clearInfo() {
+  showWarning("");
+  document.querySelector(".resultado").style.display = "none";
+}
+
+function showWarning(msg) {
+  document.querySelector(".aviso").innerHTML = msg;
 }
